23ec60961ea84270f7364550833649857cb9d2c5
chenyunshu2 <chenyunshu2@gmail.com>
2023-11-18 22:47:27 +0800

    delete product from cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -35,11 +35,13 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   Cart.getCart(cart => {
     Product.fetchAll(products => {
-      const cartProducts = []
+      const cartProducts = [];
       for (product of products) {
-        const cartProductData = cart.products.find(prod => prod.id === product.id)
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
         if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty })
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
         }
       }
       res.render('shop/cart', {
@@ -48,7 +50,6 @@ exports.getCart = (req, res, next) => {
         products: cartProducts
       });
     });
-    
   });
 };
 
@@ -58,6 +59,14 @@ exports.postCart = (req, res, next) => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
 };
 
 exports.getOrders = (req, res, next) => {
--- models/cart.js
+++ models/cart.js
@@ -1,4 +1,3 @@
-const { hasSubscribers } = require('diagnostics_channel');
 const fs = require('fs');
 const path = require('path');
 
@@ -38,35 +37,38 @@ module.exports = class Cart {
       });
     });
   }
-    
-    static deleteProduct(id, productPrice) {
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                return;
-            }
-            const updatedCart = { ...JSON.parse(fileContent) };
-            const product = updatedCart.products.find(prod => prod.id === id);
-            const productQty = product.qty;
-            updatedCart.products = updatedCart.products.filter(
-                prod => prod.id !== id
-            );
-            updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
-            fs.writeFile(p, JSON.stringify(updatedCart), err => {
-                console.log(err);
-                });
-        })
 
-    }
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find(prod => prod.id === id);
+      if (!product) {
+          return;
+      }
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(
+        prod => prod.id !== id
+      );
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
 
-    static getCart(cb) {
-        fs.readFile(p, (err, fileContent) => {
-            const cart = JSON.parse(fileContent);
-            if (err) {
-                cb(null);
-            } else {
-                cb(cart);
-            }
-            
-        })
-    }
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    });
+  }
 };
