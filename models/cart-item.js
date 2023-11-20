df2f19d12eb425dea2953dcd984e5a7c7d14f917
chenyunshu2 <chenyunshu2@gmail.com>
2023-11-20 22:55:32 +0800

    use sequelize to add product to cart and delete product from cart

--- models/cart-item.js
+++ models/cart-item.js
@@ -4,11 +4,11 @@ const Sequelize = require('sequelize');
 const sequelize = require('../util/database');
 
 const CartItem = sequelize.define('cartItem', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
     },
     quantity: Sequelize.INTEGER
 });
