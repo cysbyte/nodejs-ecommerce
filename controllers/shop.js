be773bc49801c9418a2c3d9bb4ebaaa77a756bb8
chenyunshu2 <chenyunshu2@gmail.com>
2023-11-21 23:04:04 +0800

    fetch product from mongodb

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,8 @@
 const Product = require('../models/product');
-const Order = require('../models/order');
+// const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  Product.fetchAll()
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
@@ -36,8 +36,9 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.findAll()
+  Product.fetchAll()
     .then(products => {
+      //console.log(products);
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
