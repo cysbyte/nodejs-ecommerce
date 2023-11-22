0e5e19ff13d9e0cdbcced31699e9249a7efc1f68
chenyunshu2 <chenyunshu2@gmail.com>
2023-11-22 15:54:40 +0800

    edit and update product

--- models/product.js
+++ models/product.js
@@ -2,17 +2,25 @@ const getDb = require('../util/database').getDb;
 const mongodb = require('mongodb');
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
     const db = getDb();
-    return db.collection('products')
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection('products')
+        .updateOne({_id: this._id}, {$set: this});
+    } else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
@@ -32,7 +40,7 @@ class Product {
         return products;
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
       });
   }
 
@@ -48,6 +56,18 @@ class Product {
       .catch(err=>console.log(err));
   }
 
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(result => {
+        console.log('Deleted');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
 
 module.exports = Product;
