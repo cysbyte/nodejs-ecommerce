9c1f202db567cb5a929c9160fa9e90b0aba1dc34
chenyunshu2 <chenyunshu2@gmail.com>
2023-11-23 16:38:05 +0800

    add cart to user

--- models/user.js
+++ models/user.js
@@ -1,16 +1,36 @@
-const { Sequelize } = require('sequelize');
+const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb');
 
-const sequelize = require('../util/database');
+class User {
+    constructor(username, email, cart, id) {
+        this.name = username;
+        this.email = email;
+        this.cart = cart;
+        this._id = id;
+    }
 
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-});
+    save() {
+        const db = getDb();
+        return db.collection('users').insertOne(this);
+    }
+
+    addToCart(product) {
+        // const cartProduct = this.cart.items.findIndex(cp => {
+        //     return cp._id === product._id;
+        // })
+        const updatedCart = { items: [{ ...product, quantiry: 1 }] };
+        const db = getDb();
+        return db.collection('users').updatedOne(
+            { _id: new mongodb.ObjectId(this._id) },
+            { $set: {cart: updatedCart}}
+        );
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        return db.collection('users').
+            findOne({ _id: new mongodb.ObjectId(userId) });
+    }
+}
 
 module.exports = User;
