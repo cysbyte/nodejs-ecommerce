ead0c686bc45cd92c6ee8503e0210cc542a95595
chenyunshu2 <chenyunshu2@gmail.com>
2023-11-26 21:00:29 +0800

    use bcryptjs to encrypt the password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { emit } = require('../models/user');
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
@@ -41,17 +42,21 @@ exports.postSignup = (req, res, next) => {
             if (userDoc) {
                 return res.redirect('/signup');
             }
-            const user = new User({
-                email: email,
-                password: password,
-                cart: { items: [] }
-            });
-            return user.save();
-        })
-        .then(result => {
-            res.redirect("/login");
-        })
-        .catch(err => console.log(err));
+            return bcrypt
+                .hash(password, 12)
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: email,
+                        password: hashedPassword,
+                        cart: { items: [] }
+                    });
+                    return user.save();
+                })
+                .then(result => {
+                    res.redirect("/login");
+                })
+                .catch(err => console.log(err));
+        });  
 };
 
 exports.postLogout = (req, res, next) => {
