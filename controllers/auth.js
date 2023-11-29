854609c1e81b891fda854c58a79fc1c842e8b303
chenyunshu2 <chenyunshu2@gmail.com>
2023-11-29 16:09:23 +0800

    validate email, password, confirmPassword on signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 const crypto = require('crypto');
 const User = require('../models/user');
+const { validationResult } = require('express-validator');
 
 const transporter = nodemailer.createTransport(
   sendgridTransport({
@@ -76,6 +77,16 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
+  }
+ 
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
