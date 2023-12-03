4f45da0e37a42ceada02b169efc47d9cab7ddc7d
chenyunshu2 <chenyunshu2@gmail.com>
2023-12-03 16:39:52 +0800

    use fetch to delete product asynchronously

--- controllers/admin.js
+++ controllers/admin.js
@@ -137,12 +137,14 @@ exports.getProducts = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.deleteOne({ _id: prodId, userId: req.user._id })
     .then(() => {
       console.log('DESTROYED PRODUCT');
-      res.redirect('/admin/products');
+      res.status(200).json({ message: 'Success' });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      res.status(500).json({message: 'Deleting product failed.'});
+    });
 };
